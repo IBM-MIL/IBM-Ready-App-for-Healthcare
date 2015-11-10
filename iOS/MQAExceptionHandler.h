363306a619674818ee785b0e9c5bce6e5f109f8e
Harrison <hgsaylor@us.ibm.com>
2015-11-10 13:16:04 -0600

    Important for making MQA error handling work

--- iOS/MQAExceptionHandler.h
+++ iOS/MQAExceptionHandler.h
@@ -9,5 +9,7 @@
 #ifndef MQAExceptionHandler_h
 #define MQAExceptionHandler_h
 
+volatile void exceptionHandler(NSException *exception);
+extern NSUncaughtExceptionHandler *exceptionHandlerPointer;
 
 #endif /* MQAExceptionHandler_h */
